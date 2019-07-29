83460dd7a66c57ff59bc51fe9b70e275cc0c1c5c
Richard Peng <richard@richardpeng.com>
2019-07-29 12:02:16 -0400

    Add Github integration; Refactor Redmine integration

--- slack_bot.js
+++ slack_bot.js
@@ -69,25 +69,20 @@ if (!process.env.token) {
 }
 
 var Botkit = require('./lib/Botkit.js');
-var os = require('os');
 
 var redmine = require ('./lib/integrations/redmine');
+var github = require('./lib/integrations/github')
 
 var controller = Botkit.slackbot({
   debug: false,
 });
 
-var expressions = [{
-  pattern: /#(\d+)/,
-  replacement: "https://pm.alfajango.com/issues/",
-  integration: 'redmine'
-}];
+const integrations = [
+  redmine,
+  github
+]
 
-var integrations = {
-  redmine: redmine
-};
-
-var bot = controller.spawn({
+controller.spawn({
   token: process.env.token
 }).startRTM();
 
@@ -97,7 +92,7 @@ controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', funct
     timestamp: message.ts,
     channel: message.channel,
     name: 'robot_face',
-  }, function(err, res) {
+  }, function(err) {
     if (err) {
       bot.botkit.log('Failed to add emoji reaction :(', err);
     }
@@ -112,175 +107,16 @@ controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', funct
   });
 });
 
-controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-  var name = message.match[1];
-  controller.storage.users.get(message.user, function(err, user) {
-    if (!user) {
-      user = {
-        id: message.user,
-      };
-    }
-    user.name = name;
-    controller.storage.users.save(user, function(err, id) {
-      bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-    });
-  });
-});
-
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-  controller.storage.users.get(message.user, function(err, user) {
-    if (user && user.name) {
-      bot.reply(message, 'Your name is ' + user.name);
-    } else {
-      bot.startConversation(message, function(err, convo) {
-        if (!err) {
-          convo.say('I do not know your name yet!');
-          convo.ask('What should I call you?', function(response, convo) {
-            convo.ask('You want me to call you `' + response.text + '`?', [
-              {
-                pattern: 'yes',
-                callback: function(response, convo) {
-                  // since no further messages are queued after this,
-                  // the conversation will end naturally with status == 'completed'
-                  convo.next();
-                }
-              },
-              {
-                pattern: 'no',
-                callback: function(response, convo) {
-                  // stop the conversation. this will cause it to end with status == 'stopped'
-                  convo.stop();
-                }
-              },
-              {
-                default: true,
-                callback: function(response, convo) {
-                  convo.repeat();
-                  convo.next();
-                }
-              }
-            ]);
-
-            convo.next();
-
-          }, {'key': 'nickname'}); // store the results in a field called nickname
-
-          convo.on('end', function(convo) {
-            if (convo.status == 'completed') {
-              bot.reply(message, 'OK! I will update my dossier...');
-
-              controller.storage.users.get(message.user, function(err, user) {
-                if (!user) {
-                  user = {
-                    id: message.user,
-                  };
-                }
-                user.name = convo.extractResponse('nickname');
-                controller.storage.users.save(user, function(err, id) {
-                  bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                });
-              });
-
-            } else {
-              // this happens if the conversation ended prematurely for some reason
-              bot.reply(message, 'OK, nevermind!');
-            }
-          });
-        }
-      });
-    }
-  });
-});
-
-controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-  bot.startConversation(message, function(err, convo) {
-
-    convo.ask('Are you sure you want me to shutdown?', [
-      {
-        pattern: bot.utterances.yes,
-        callback: function(response, convo) {
-          convo.say('Bye!');
-          convo.next();
-          setTimeout(function() {
-            process.exit();
-          }, 3000);
-        }
-      },
-      {
-        pattern: bot.utterances.no,
-        default: true,
-        callback: function(response, convo) {
-          convo.say('*Phew!*');
-          convo.next();
-        }
-      }
-    ]);
-  });
-});
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-  'direct_message,direct_mention,mention', function(bot, message) {
-
-    var hostname = os.hostname();
-    var uptime = formatUptime(process.uptime());
-
-    bot.reply(message,
-      ':robot_face: I am a bot named <@' + bot.identity.name +
-      '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
-  });
-
 controller.on('direct_message,direct_mention,mention,ambient', function(bot, message) {
+  message.unfurl_links = false;
   if (message.text) {
-    expressions.forEach(function(expression) {
-      var regex = new RegExp(expression.pattern.source, 'gm');
-      var matches = message.text.match(regex);
-      if (matches) {
-        var captureRegex = new RegExp(expression.pattern.source);
-        var integrationResponses = matches.map(match => new Promise((resolve, reject) => {
-          var replacement = match.match(captureRegex);
-          if (replacement) {
-            var capture = replacement[1];
-            var response = expression.replacement + capture;
-            if (expression.integration && integrations[expression.integration]) {
-              integrations[expression.integration](capture, function(integrationResponse) {
-                if (!integrationResponse) {
-                  resolve();
-                } else {
-                  integrationResponse.title_link = response;
-                  resolve(integrationResponse);
-                }
-              });
-            } else {
-              resolve(response);
-            }
-        }}));
-        Promise.all(integrationResponses).then(attachments => {
-          // console.log(attachments);
-          message.unfurl_links = false;
-          bot.reply(message, {attachments})
-        });
-      }
-    });
+    const queries = integrations.reduce((arr, integration) => {
+      return integration ? arr.concat(integration(message.text)) : arr
+    }, [])
+    Promise.all(queries)
+      .then(values => {
+        const attachments = values.sort((a, b) => (a.index > b.index) ? 1 : -1)
+        bot.reply(message, {attachments})
+      })
   }
 });
-
-function formatUptime(uptime) {
-  var unit = 'second';
-  if (uptime > 60) {
-    uptime = uptime / 60;
-    unit = 'minute';
-  }
-  if (uptime > 60) {
-    uptime = uptime / 60;
-    unit = 'hour';
-  }
-  if (uptime != 1) {
-    unit = unit + 's';
-  }
-
-  uptime = uptime + ' ' + unit;
-  return uptime;
-}
